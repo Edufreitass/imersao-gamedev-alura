0aad437c1bb61effcffd0bca45d0102b33e59e1e
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-06-23 15:49:56 -0300

    Aula 2 Inserindo pulo duplo, efeitos sonoros e colisão

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,43 +1,48 @@
-class Personagem {
+class Personagem extends Animacao {
 
-    constructor(imagem) {
-        this.imagem = imagem;
-        this.matriz = [
-            [0, 0],
-            [220, 0],
-            [440, 0],
-            [660, 0],
-            [0, 270],
-            [220, 270],
-            [440, 270],
-            [660, 270],
-            [0, 540],
-            [220, 540],
-            [440, 540],
-            [660, 540],
-            [0, 810],
-            [220, 810],
-            [440, 810],
-            [660, 810],
-        ]
-        this.frameAtual = 0;
+    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+
+        this.yInicial = height - this.altura;
+        this.y = this.yInicial;
+
+        this.velocidadeDoPulo = 0;
+        this.gravidade = 3;
     }
 
-    exibe() {
-        // image(1°img, 2ºeixoX, 3°eixoY, 4°largura, 5°altura, posicaoMapearImg(eixoX, eixoY, largura, altura))
-        image(this.imagem, 0, height - 135, 110, 135,
-            this.matriz[this.frameAtual][0], this.matriz[this.frameAtual][1], 220, 270);
-
-        this.anima();
+    pula() {
+        this.velocidadeDoPulo = - 30;
     }
 
-    anima() {
-        this.frameAtual++;
+    aplicaGravidade() {
+        this.y = this.y + this.velocidadeDoPulo;
+        this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
 
-        if (this.frameAtual >= this.matriz.length - 1) {
-            this.frameAtual = 0;
+        if (this.y > this.yInicial) {
+            this.y = this.yInicial;
         }
+    }
 
+    estaColidindo(inimigo) {
+        // noFill();
+        // rect(this.x, this.y, this.largura, this.altura);
+        // rect(inimigo.x,
+        //     inimigo.y,
+        //     inimigo.largura,
+        //     inimigo.altura);
+            const precisao = .7;
+        const colisao = collideRectRect(
+            this.x, 
+            this.y, 
+            this.largura * precisao,
+            this.altura * precisao,
+            inimigo.x,
+            inimigo.y,
+            inimigo.largura * precisao,
+            inimigo.altura * precisao,
+            );
+
+            return colisao;
     }
 
 }
