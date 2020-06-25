8b81d253135b18164186482b89fe5741c0cefb0c
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-06-24 22:20:02 -0300

    Aula 3 - Adicionando novos inimigos e pontuação

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,17 +1,23 @@
 class Personagem extends Animacao {
 
-    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
 
-        this.yInicial = height - this.altura;
+        this.variacaoY = variacaoY;
+        this.yInicial = height - this.altura - 30;
         this.y = this.yInicial;
 
         this.velocidadeDoPulo = 0;
-        this.gravidade = 3;
+        this.gravidade = 6;
+        this.alturaDoPulo = -50;
+        this.pulos = 0;
     }
 
     pula() {
-        this.velocidadeDoPulo = - 30;
+        if (this.pulos < 2) {
+            this.velocidadeDoPulo = this.alturaDoPulo;
+            this.pulos++;
+        }
     }
 
     aplicaGravidade() {
@@ -20,6 +26,7 @@ class Personagem extends Animacao {
 
         if (this.y > this.yInicial) {
             this.y = this.yInicial;
+            this.pulos = 0;
         }
     }
 
@@ -30,19 +37,19 @@ class Personagem extends Animacao {
         //     inimigo.y,
         //     inimigo.largura,
         //     inimigo.altura);
-            const precisao = .7;
+        const precisao = .7;
         const colisao = collideRectRect(
-            this.x, 
-            this.y, 
+            this.x,
+            this.y,
             this.largura * precisao,
             this.altura * precisao,
             inimigo.x,
             inimigo.y,
             inimigo.largura * precisao,
             inimigo.altura * precisao,
-            );
+        );
 
-            return colisao;
+        return colisao;
     }
 
 }
